4233cd72f24bf6d63115858d5574e8049c3e1169
kadelyin <23123kw@kuranuicollege.school.nz>
2026-06-22 08:58:02 +1200

    version2-quiz.js

--- version2-quiz.js
+++ version2-quiz.js
@@ -10,11 +10,14 @@
 const prompt = require("prompt-sync")();
 
 // helpers
-const debugToggle = false;
+const debugToggle = true;
 function debugPrint(string) {
   // easy way to toggle whether or not i want to have debugging in my code.
   if (!debugToggle) return;
-  console.log(`\x1b[2mDEBUG: ${string}\x1b[0m`); // this \x1b[1m stuff makes console.log strings bold based on the value given e.g. [1m.
+
+  console.log(`\x1b[2mDEBUG: ${string}\x1b[0m`);
+  // this \x1b[1m stuff makes console.log strings bold based on the value given e.g. [1m
+  // (https://dev.to/atomikjaye/styling-consolelog-in-the-terminal-25c1)
 }
 
 const shuffleToggle = true;
@@ -24,22 +27,25 @@ function shuffleArray(array) {
 
   let shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
+    // pick a random index from 0 to i
     const j = Math.floor(Math.random() * (i + 1));
+    // swap elements array[i] and array[j]
     [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
   }
   return shuffled;
 }
 
 function cleanValue(value) {
+  // a function I made that takes a value and changes it to a correct value, so from "y" > true.
   let cleanedValue = value.trim().toLowerCase();
-  if (cleanedValue === "y") return true;
-  if (cleanedValue === "n") return false;
-  return false;
+  if (cleanedValue == "y") return true;
+  if (cleanedValue == "n") return false;
 }
 
 // components
-const welcomeToggle = true;
+const welcomeToggle = false;
 function welcomeComponent() {
+  // welcomeComponent that introduces the player and returns values (name, dataSaving)
   if (welcomeToggle) {
     console.log(
       "Welcome to the L&P quiz! Start by answering the following questions.",
@@ -53,15 +59,26 @@ function welcomeComponent() {
 
     console.log(`Hello ${name}!`);
 
-    // let dataSaving = "";
-    // while (dataSaving.trim().toLowerCase() !== 'y' && dataSaving.trim().toLowerCase() !== 'n') {
-    //     dataSaving = prompt("Do you want your progress to save? (y/n) : ")
-    // }
-    // debugPrint(`dataSaving == ${dataSaving}`);
+    let dataSaving = "n"; // defaulted at "n" so it is never empty if the while loop is skipped.
+    let dataAskToggle = false;
+    if (dataAskToggle) {
+      dataSaving = "";
+      while (
+        dataSaving.trim().toLowerCase() !== "y" &&
+        dataSaving.trim().toLowerCase() !== "n"
+      ) {
+        dataSaving = prompt("Do you want your progress to save? (y/n) : ");
+        /* this WHOLE part was just an idea for saving the player's data so there could be leaderboards or personal bests.
+        but at this moment I don't know how to save data through javascript. 
+        something to do with another service or dependancy that logs stuff into a json file.*/
+      }
+    }
+    debugPrint(`dataSaving == ${dataSaving}`);
 
     return {
+      // returns multiple values
       name: name.trim(),
-      dataSaving: cleanValue(dataSaving),
+      dataSaving: cleanValue(dataSaving), // cleans value from "y" > true, making it easier to recognise the value and use.
     };
   }
   return {
@@ -74,19 +91,23 @@ function quizComponent(questions) {
   let score = 0;
   const activeQuestions = shuffleArray(questions);
 
-  console.log(`\x1b[1mQUIZ BEGINS NOW!\x1b[0m`);
+  console.log(`\x1b[1mQUIZ BEGINS NOW!\x1b[0m`); // another use of bold console.log text.
 
   for (let i = 0; i < activeQuestions.length; i++) {
     const current = activeQuestions[i];
     console.log(`Question ${i + 1}: ${current.question}`);
 
-    current.choices.forEach((choice) => console.log(choice)); // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+    current.choices.forEach((choice) => console.log(choice));
+    /*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+    The forEach() method of Array instances executes a provided function once for each array element.*/
 
     let userAnswer = "";
     const validAnswers = ["a", "b", "c", "d"];
 
     while (!validAnswers.includes(userAnswer)) {
-      // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes)
+      /* (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes)
+      The includes() method of Array instances determines whether an array includes a certain value among 
+      its entries, returning true or false as appropriate.*/
       userAnswer = prompt("Your answer (A, B, C, or D) : ")
         .trim()
         .toLowerCase();
@@ -117,7 +138,7 @@ const quizQuestions = [
   },
   {
     question:
-      "L&P was originally created by mixing fresh lemon juice with what local ingredient?",
+      "L&P was originally created by mixing lemon juice with what ingredient?",
     choices: [
       "A) Naturally carbonated mineral spring water",
       "B) Volcanic mud extract",
@@ -127,58 +148,58 @@ const quizQuestions = [
     answer: "a",
   },
   {
-    question: "In which decade was L&P officially created and introduced?",
+    question: "In what decade was L&P officially created?",
     choices: ["A) 1880s", "B) 1900s", "C) 1940s", "D) 1970s"],
     answer: "b",
   },
   {
-    question: "Which global beverage company owns and manufactures L&P today?",
+    question: "Which beverage company owns and makes L&P today?",
     choices: [
-      "A) Funcor Suntory",
+      "A) Frucor Suntory",
       "B) PepsiCo",
       "C) The Coca-Cola Company",
-      "Whittakers",
+      "D) Whittakers",
     ],
     answer: "c",
   },
   {
-    question:
-      "How tall is the famous giant L&P bottle statue located in the town of Paeroa?",
+    question: "How tall is the giant L&P bottle statue in the town of Paeroa?",
     choices: [
       "A) 3.5 metres",
-      "B) 6.8 metres",
+      "B) 20.5 metres",
       "C) 12.2 metres",
-      "D) 20.5 metres",
+      "D) 6.8 metres",
     ],
-    answer: "b",
+    answer: "d",
   },
 ];
 
 // init
 const { name, dataSaving } = welcomeComponent();
-/* otherwis known as...
+/* otherwise known as...
 const component = welcomeComponent();
 const name = component.name;
-const dataSaving = component.dataSaving;
-*/
+const dataSaving = component.dataSaving;*/
 debugPrint(`userAnswers = { name : ${name}, dataSaving: ${dataSaving} }`);
 
 while (true) {
-  const finalScore = quizComponent(quizQuestions);
+  const score = quizComponent(quizQuestions);
 
   console.log(
-    `\n Thanks for playing ${name || "null"}. Your final score was ${finalScore}/${quizQuestions.length}.`,
+    `\nThanks for playing ${name || "null"}. Your final score was ${score}/${quizQuestions.length}.`,
   );
 
   let userChoice = "";
   while (userChoice !== "y" && userChoice !== "n") {
-    userChoice = prompt("Would you like to play again? : ")
+    userChoice = prompt("Would you like to play again? (y/n) : ")
       .trim()
       .toLowerCase();
   }
 
   if (userChoice == "n") {
-    console.log(`Goodbye ${name}. Thanks for playing the L&P quiz.`);
+    console.log(
+      `Goodbye ${name || "Player"}. Thanks for playing the L&P quiz.`,
+    );
     break;
   }
 }
